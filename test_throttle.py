aea0e6669cea4434a71249336a2bfe1727f1274a
Chen <chenw@cmu.edu>
2014-12-07 12:32:18 +0000

    fix saving candidate servers bug

--- test_throttle.py
+++ test_throttle.py
@@ -11,43 +11,43 @@ video = 'BBB'
 
 client = sys.argv[1]
 # client = 'porto'
-# cache_agent = 'agens-05'
+cache_agent = 'agens-04'
 
 ## Determine the cache agent from all available agents
-server_ips = get_available_srvs()
-all_cache_agents = []
+#server_ips = get_available_srvs()
+#all_cache_agents = []
 # ping all servers and get the mean RTT
-print "=========== Pinging All Agents ============="
-srv_rtts = {}
-for srv in server_ips.keys():
-        if "cache-agent" in srv:
-                print "=========== Pinging " + srv + "  ============="
-                rtt = getRTT(server_ips[srv], 5)
-                mnRtt = sum(rtt) / float(len(rtt))
-                srv_rtts[srv] = mnRtt
-		all_cache_agents.append(srv)
-
-print "All available cache agents are: "
-print all_cache_agents
-
-sorted_rtts = sorted(srv_rtts.items(), key=operator.itemgetter(1))
-cache_agent = sorted_rtts[0][0]
-
+#print "=========== Pinging All Agents ============="
+#srv_rtts = {}
+#for srv in server_ips.keys():
+#        if "cache-agent" in srv:
+#                print "=========== Pinging " + srv + "  ============="
+#                rtt = getRTT(server_ips[srv], 5)
+#                mnRtt = sum(rtt) / float(len(rtt))
+#                srv_rtts[srv] = mnRtt
+#		all_cache_agents.append(srv)
+#
+#print "All available cache agents are: "
+#print all_cache_agents
+#
+#sorted_rtts = sorted(srv_rtts.items(), key=operator.itemgetter(1))
+#cache_agent = sorted_rtts[0][0]
+#
 print "=============== Cache Agent for Client: ", client, " is ", cache_agent, " ======================"
 
-expNum = 10
+expNum = 1
 clientCandidates = {}
 clientCandidates["cache-agent"] = cache_agent
 
+candidates = ['cache-agent-04', 'cache-agent-05']
+
 for i in range(1, expNum + 1):
-	candidates = random.sample(all_cache_agents, 2)
 	expID = 'exp' + str(i)
 	clientID = client + "-" + expID
 	print "Selected candidate servers for ", clientID, " are :"
 	print candidates
 	test_client_agent(clientID, cache_agent, candidates, port, video)
 	clientCandidates[expID] = candidates
-	# sleep(random.randint(10, 100))
 
 candidateFile = "./data/" + client + "-candidates.json"
 with open(candidateFile, 'w') as outfile:
