cdde37eb69ae87862db948b0717f698cda225df9
ephemeral2eternity <chenw@andrew.cmu.edu>
2014-12-14 05:02:12 +0000

    update ping method as ping to customized pingServer

--- ping.py
+++ ping.py
@@ -4,8 +4,27 @@ PING a server with count times and get the RTT list
 from subprocess import Popen, PIPE
 import string
 import re
+import socket
+import time
 
 # server_ip = '104.155.15.0'
+PING_PORT = 8717
+
+def ping(ip):
+    # Connect to the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, PING_PORT))
+    # Send a PING message
+    message = 'PING'
+    ts_sent = time.time()
+    len_sent = s.send(message)
+    # Receive a response
+    response = s.recv(len_sent)
+    ts_recv = time.time()
+    ## Compute rtt
+    rtt = (ts_recv - ts_sent) * 1000
+    s.close()
+    return rtt
 
 def extract_number(s,notfound='NOT_FOUND'):
     regex=r'[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?'
@@ -15,11 +34,19 @@ def getRTT(ip, count):
 	'''
 	Pings a host and return True if it is available, False if not.
 	'''
-	cmd = ['ping', '-c', str(count), ip]
-	process = Popen(cmd, stdout=PIPE, stderr=PIPE)
-	stdout, stderr = process.communicate()
-	rttList = parsePingRst(stdout, count)
+	# cmd = ['ping', '-c', str(count), ip]
+	# process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+	# stdout, stderr = process.communicate()
+	# rttList = parsePingRst(stdout, count)
 	# print rttList
+
+	# Use customized ping server to answer
+	rttList = []
+	for i in range(count):
+		cur_rtt = ping(ip)
+		rttList.append(cur_rtt)
+		time.sleep(0.05)
+		
 	return rttList
 
 def parsePingRst(pingString, count):
